365bfd9c3370e7710d0c37d0a1358a7d4d05fc00
ambrosa <aleambro@gmail.com>
2011-11-13 11:17:24 +0100

    add package_data

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-
 from distutils.core import setup, Extension
 
 pkg = 'Extensions.SimpleUmount'
 setup (name = 'enigma2-plugin-extensions-simpleumount',
-       version = '0.04',
-       description = 'Simple umounter mass storage device',
-       package_dir = {pkg: 'plugin'},
-       packages = [pkg]
-       )
+	version = '0.06',
+	description = 'Simple umounter mass storage device',
+	package_dir = {pkg: 'plugin'},
+	packages = [pkg],
+	package_data = {pkg: ['po/*/LC_MESSAGES/*.mo']}
+	)
 
