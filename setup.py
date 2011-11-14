18bb931c05754294ac9b37b6d7547b88d8b97c96
ambrosa <aleambro@gmail.com>
2011-11-14 18:36:43 +0100

    upgraded setup.py to 0.08

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 pkg = 'Extensions.SimpleUmount'
 setup (name = 'enigma2-plugin-extensions-simpleumount',
-	version = '0.07',
+	version = '0.08',
 	description = 'Simple umounter mass storage device',
 	package_dir = {pkg: 'plugin'},
 	packages = [pkg],
