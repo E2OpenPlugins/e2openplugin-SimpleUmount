c662f8767f3c60902e6c2b87fd2900199742bb15
persianpros <persianpros@yahoo.com>
2021-04-14 15:28:15 +0430

    PEP8 double aggressive E22, E224, E241, E242 and E27

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -182,7 +182,7 @@ class SimpleUmount(Screen):
 					# add entry in device list
 					if config.plugins.simpleumount.showonlyremovable.value == 0 or removable == '1':
 						self.list_dev.append(l[0])
-						self.wdg_list_dev.append("%-10s %-14s %-11s %8s MiB" % (l[0], l[1], l[2]+','+l[3][:2], size))
+						self.wdg_list_dev.append("%-10s %-14s %-11s %8s MiB" % (l[0], l[1], l[2] + ',' + l[3][:2], size))
 
 		if len(self.list_dev) == 0:
 			self.noDeviceError = True
